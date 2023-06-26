56f2fdcabf3717f800345e14110d1aee1e5afca8
ranudi123 <108453143+ranudi123@users.noreply.github.com>
2023-06-26 22:28:22 +0530

    Add files via upload

---
+++ Lab 06-29897/Q3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int number,i;
+    int factorial=1;
+
+    printf("Enter a number: ");
+    scanf("%d",&number);
+
+    if(number<0)
+    {
+        printf("Not calculatable\n");
+    }
+    else if(number==0)
+    {
+        printf("Factorial of %d is %d \n",number,1);
+    }
+    else
+    {
+        for(i=1;i<=number;i++)
+        {
+            factorial *= i;
+        }
+
+        printf("Factorial of %d is %d \n",number,factorial);
+    }
+    return 0;
+}
